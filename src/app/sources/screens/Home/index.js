87c0c108d551a2bee25aa05beec1d0ceb82eaf38
Minh Van Nguyen <van23112002@gmail.com>
2022-11-30 23:08:14 +0700

    add fonts

--- src/app/sources/screens/Home/index.js
+++ src/app/sources/screens/Home/index.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { ImageBackground, View, Text, Image, Pressable } from "react-native";
+import LinearGradient from 'react-native-linear-gradient';
 
 import styles from "./styles";
 
 function Home({ navigation }) {
     return (
         <View style={styles.container}>
-            <Image source='/assets/logo.png' alt='logo' />
-            <Text>Home</Text>
+            <ImageBackground source={require('../../../assets/images/bg-1.png')} resizeMode="cover" style={styles.bg}>
+                <Image source={require('../../../assets/images/logo-w.png')}  alt='logo' />
+                <Pressable style={styles.btn}>
+                    <LinearGradient start={{x: 0.5, y: 0.5}} end={{x: 0.93, y: 0.5}} colors={['#EB0327', '#EB0349']} style={styles.btn_m}>
+                        <Text style={styles.btn_text}>SIGN IN</Text>
+                    </LinearGradient>
+                </Pressable>
+            </ImageBackground>
         </View>
     )
 }
--- src/app/sources/screens/Home/styles.js
+++ src/app/sources/screens/Home/styles.js
@@ -2,9 +2,34 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    bg: {
         flex: 1,
-        alignItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    btn: {
+        position: 'absolute',
+        bottom: 12
+    },
+    btn_m: {
+        width: 356,
+        borderRadius: 6,
+        height: 55,
         justifyContent: 'center'
+    },
+    btn_text: {
+        alignSelf: 'center',
+        fontFamily: 'Poppins',
+        fontFamily: "Poppins",
+        fontStyle: "normal",
+        fontWeight: "700",
+        fontSize: 20,
+        lineHeight: 30,
+        textAlign: "center",
+        letterSpacing: 0.4,
+        color: "#FFFFFF"
     }
 });
 
