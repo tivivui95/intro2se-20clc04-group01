92985e757795379727b5438ab0f150b8c3998503
20127304_Trần Quang An Quốc <tranquanganquoc@gmail.com>
2022-12-09 10:41:46 +0700

    Home Screen code

--- src/app/sources/screens/HomeScreen/index.js
+++ src/app/sources/screens/HomeScreen/index.js
@@ -1,13 +1,45 @@
 import React, { useState } from "react";
-import { ImageBackground, View, Text, Image, Pressable, TextInput } from "react-native";
+import { ImageBackground, View, Text, Image, Pressable, TextInput, FlatList, ListViewComponent } from "react-native";
 import LinearGradient from 'react-native-linear-gradient';
 
 import styles from "./styles";
 
 const HomeScreen = ({ navigation }) => {
+
+    return(
     <View style={styles.container}>
-        <ImageBackground source={require('../../../assets/images/bg-2.png')} resizeMode="cover" style={styles.bg}></ImageBackground>
+        <Text style={styles.sayHello}>Hello
+            <Text style={styles.name}> Toshiro</Text>
+        </Text>
+        <Image 
+            style={styles.ava_picture}
+            source={require('../../../assets/images/profile.png')}
+        />
+        <Text style={styles.title}>Let's start your day</Text>
+        <TextInput  style={styles.search_wrapper} 
+                    placeholder={'Search exercise...'}/>
+
+        <View>
+            <Text style={styles.title_exercise}>Today Workout Plan</Text>
+            <Image 
+            style={styles.picture}
+            source={require('../../../assets/images/squat.png')}
+            />
+            <Image 
+            style={styles.picture}
+            source={require('../../../assets/images/pushup.png')}
+            />
+        </View>
+
+        <View>
+            <Text style={styles.title_exercise}>Category</Text> 
+        </View>
+
+        <View>
+            <Text style={styles.title_exercise}>In-progress</Text>
+        </View>
     </View>
+    )
 }
 
 export default HomeScreen;
--- src/app/sources/screens/HomeScreen/styles.js
+++ src/app/sources/screens/HomeScreen/styles.js
@@ -1,13 +1,75 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+
     container: {
+        backgroundColor: '#F8E9ED',
+        width: 414,
+        height: 852,
         flex: 1
     },
-    bg: {
-        flex: 1,
+    sayHello: {
+        position: 'relative',
+        fontSize: 20,
+        top: 46,
+        marginLeft: 32
+    },
+    name: {
+        position: 'relative',
+        fontSize: 20,
+        color: '#BB2649',
+    },
+    title: {
+        position: 'relative',
+        color: '#BB2649',
+        fontSize: 25,
+        fontWeight: "bold",
+        paddingLeft: 32,
+    },
+    ava_picture:
+    {
+        position: 'relative',
+        resizeMode: 'contain',
+        top: 25,
+        left: 334
+    },
+    plan_picture:
+    {
+        position: 'relative',
+        marginHorizontal: 25
+    },
+    search_wrapper: {
+        position: 'relative',
+        width: 300,
+        height: 50,
+        marginTop: 10,
+        marginLeft: 30,
+        paddingHorizontal: 20,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+        elevation: 5
+    },
+    title_exercise: {
+        position: 'relative',
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000000',
+        marginTop:10,
+        marginBottom: 150,
+        marginLeft: 32
+    }, 
+    name_exercise: {
+        position: 'relative',
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#FFFFFF'
+    },
+    exercise_wrapper: {
+
     }
 });
 
