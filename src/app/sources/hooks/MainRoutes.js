c574e53a9f0433f0f9a688af617964395630d68a
20127304_Trần Quang An Quốc <tranquanganquoc@gmail.com>
2022-12-23 14:08:19 +0700

    Update yarn.lock

--- src/app/sources/hooks/MainRoutes.js
+++ src/app/sources/hooks/MainRoutes.js
@@ -19,7 +19,7 @@ const Stack = createNativeStackNavigator();
 function MainRoutes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="EnterBioHW">
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
